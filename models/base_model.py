5f56d76228cc159bcce02ab8b87bfbaed3b05408
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-28 09:16:54 +0100

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,17 @@
 from datetime import datetime
 import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
+
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Bae = object
 
 
 class BaseModel:
