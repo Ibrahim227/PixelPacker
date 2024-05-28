53a347aca2aef79ff4f717cee1a96f894ea32c8d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-28 09:16:19 +0100

    update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,23 @@
 import models
 from os import getenv
 from models.base_model import Base
+from models.User import User
+from models.Images import Images
+from models.Address import Address
+from models.CompressedImages import CompressedImages
+from models.CompressionParameters import CompressionParameters
 import sqlalchemy
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+classes = {
+    "User": User,
+    "Images": Images,
+    "Address": Address,
+    "CompressedImages": CompressedImages,
+    "CompressionParameters": CompressionParameters
+}
 
 
 class DBStorage:
@@ -18,8 +32,14 @@ class DBStorage:
         PIXELPACKER_MYSQL_HOST = getenv('PIXELPACKER_MYSQL_HOST')
         PIXELPACKER_MYSQL_DB = getenv('PIXELPACKER_MYSQL_DB')
         PIXELPACKER_ENV = getenv('PIXELPACKER_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(PIXELPACKER_MYSQL_USER, PIXELPACKER_MYSQL_PWD,
-                                                                           PIXELPACKER_MYSQL_HOST, PIXELPACKER_MYSQL_DB))
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(PIXELPACKER_MYSQL_USER, PIXELPACKER_MYSQL_PWD,
+                                                 PIXELPACKER_MYSQL_HOST, PIXELPACKER_MYSQL_DB))
         if PIXELPACKER_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
+    def all(self, cls=None):
+        """query on current db"""
+        new_dict = {}
+        for k in classes:
+            pass
