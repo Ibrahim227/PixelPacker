6d56b29f611f25ef92d0a0faa69f0e41fa5360b5
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 17:29:26 +0100

    update models/Address

--- models/Address.py
+++ models/Address.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import relationship
 
-class Address:
-    pass
+import models
+
+
+class Address(BaseModel, Base):
+    """Represents an address of a user"""
+    __tablename__ = 'address'
+    street_id = Column(Integer, primary_key=True)
+    street = Column(Integer, nullable=False)
+    city = Column(String(255), nullable=False)
+    state = Column(String(255), nullable=False)
+    postal_code = Column(Integer, nullable=False)
