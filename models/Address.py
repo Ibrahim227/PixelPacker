afd4bd6e53ed2b0171b9e06830addbce21fe96cf
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 17:32:50 +0100

    update models/Address

--- models/Address.py
+++ models/Address.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
-from sqlalchemy import relationship
 
-import models
+from models.base_model import BaseModel, Base
 
 
 class Address(BaseModel, Base):
