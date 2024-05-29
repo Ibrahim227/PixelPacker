0d421b73814999434175719979aa8c7c54d96d66
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 18:15:09 +0100

    update models/Images

--- models/Images.py
+++ models/Images.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
 
 import models
 from models.base_model import BaseModel, Base
@@ -8,17 +8,21 @@ from models.base_model import BaseModel, Base
 
 class Images(BaseModel, Base):
     """Represents Images properties"""
-    __tablename__ = 'images'
-    image_id = Column(Integer, primary_key=True)
-    original_filename = Column(String(255), nullable=False)
-    upload_date = Column(nullable=False)
-    original_size = Column(Integer, nullable=False)
-    width = Column(Integer, nullable=False)
-    height = Column(Integer, nullable=False)
-    color_space = Column(String(255), nullable=False)
-
-    if models.storage_t != "db":
-        pass
+    if models.storage_t == 'db':
+        __tablename__ = 'images'
+        image_id = Column(Integer, primary_key=True)
+        original_filename = Column(String(255), nullable=False)
+        upload_date = Column(DateTime, nullable=False)
+        original_size = Column(Integer, nullable=False)
+        width = Column(Integer, nullable=False)
+        height = Column(Integer, nullable=False)
+        color_space = Column(String(255), nullable=False)
+    else:
+        image_id = ""
+        original_filename = ""
+        upload_date = ""
+        original_size = ""
+        color_space = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize Images"""
