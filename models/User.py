8b166c4b8ee89f3c7df5190a7e13e7d64426be10
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 17:19:41 +0100

    update models/User

--- models/User.py
+++ models/User.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
+from hashlib import md5
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import relationship
+
+import models
 
 
-class User:
-    pass
+class User(BaseModel, Base):
+    """Represents a user"""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        id = Column(Integer, primary_key=True, auto_increment=True)
+        first_name = Column(String(255), nullable=True)
+        last_name = Column(String(255), nullable=True)
+        email = Column(String(255), nullable=False)
+        password = Column(String(255), nullable=False)
+        phone = Column(Integer, nullable=False)
+        address = relationship("Address", backref="user")
+    else:
+        password = ""
+        email = ""
+        last_name = ""
+        first_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """set a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+            super().__setattr__(name, value)
