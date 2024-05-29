3ca03f7a0c027009c269d2fd722cefdf42c61251
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 17:19:57 +0100

    update models/Images

--- models/Images.py
+++ models/Images.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
+from sqlalchemy import Column, String, Integer
+
+import models
+from models.base_model import BaseModel, Base
 
 
-class Images:
-    pass
+class Images(BaseModel, Base):
+    """Represents Images properties"""
+    __tablename__ = 'images'
+    image_id = Column(Integer, primary_key=True)
+    original_filename = Column(String(255), nullable=False)
+    upload_date = Column(nullable=False)
+    original_size = Column(Integer, nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    color_space = Column(String(255), nullable=False)
+
+    if models.storage_t != "db":
+        pass
