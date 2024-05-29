1e5a324241d09de63ea86d5b67b23e6befa1d71d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-29 17:20:14 +0100

    update models/CompressedImages

--- models/CompressedImages.py
+++ models/CompressedImages.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, BLOB
+from sqlalchemy import relationship
 
-class CompressedImages:
-    pass
+import models
+
+
+class CompressedImages(BaseModel, Base):
+    """Represents the Compressed image"""
+    __tablename__ = 'compressedimages'
+    compressed_images_id = Column(Integer, primary_key=True)
+    image_id = Column(Integer, nullable=False)
+    compressed_size = Column(Integer, nullable=False)
+    compressed_ratio = Column(Float, nullable=False)
+    compressed_filename = Column(String(255), nullable=False)
+    compressed_data = Column(BLOB, nullable=False)
+    
+
