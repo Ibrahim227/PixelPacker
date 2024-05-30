aa9dcfae38cd2b36c12bca070bef643f5da3970d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-30 07:56:23 +0100

    update models/Address

--- models/Address.py
+++ models/Address.py
@@ -7,14 +7,21 @@ from models.base_model import BaseModel, Base
 
 class Address(BaseModel, Base):
     """Represents an address of a user"""
-    __tablename__ = 'address'
-    street_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    street = Column(Integer, nullable=False)
-    city = Column(String(255), nullable=False)
-    state = Column(String(255), nullable=False)
-    postal_code = Column(Integer, nullable=False)
-    user = relationship("User", back_populates="address")
+    if models.storage_t == 'db':
+        __tablename__ = 'address'
+        street_id = Column(Integer, primary_key=True, autoincrement=True)
+        user_id = Column(Integer, ForeignKey('user.id'))
+        street = Column(Integer, nullable=False)
+        city = Column(String(255), nullable=False)
+        state = Column(String(255), nullable=False)
+        postal_code = Column(Integer, nullable=False)
+        user = relationship("User", back_populates="address")
+    else:
+        street_id = ""
+        street = ""
+        city = ""
+        state = ""
+        postal_code = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize the Address"""
