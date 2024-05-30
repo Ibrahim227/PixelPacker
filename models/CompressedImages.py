838f67b595a6bfada15d3812c4b2e17a13e9f227
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-30 07:56:01 +0100

    update models/CompressedImages

--- models/CompressedImages.py
+++ models/CompressedImages.py
@@ -2,20 +2,28 @@
 """Import required libraries/modules"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, BLOB
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 import models
 
 
 class CompressedImages(BaseModel, Base):
     """Represents the Compressed image"""
-    __tablename__ = 'compressedimages'
-    compressed_images_id = Column(Integer, primary_key=True)
-    image_id = Column(Integer, nullable=False)
-    compressed_size = Column(Integer, nullable=False)
-    compressed_ratio = Column(Float, nullable=False)
-    compressed_filename = Column(String(255), nullable=False)
-    compressed_data = Column(BLOB, nullable=False)
+    if models.storage_t == 'db':
+        __tablename__ = 'compressedimages'
+        compressed_images_id = Column(Integer, primary_key=True)
+        image_id = Column(Integer, nullable=False)
+        compressed_size = Column(Integer, nullable=False)
+        compressed_ratio = Column(Float, nullable=False)
+        compressed_filename = Column(String(255), nullable=False)
+        compressed_data = Column(BLOB, nullable=False)
+    else:
+        compressed_images_id = ""
+        image_id = ""
+        compressed_size = ""
+        compressed_ratio = ""
+        compressed_filename = ""
+        compressed_data = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize CompressedImages"""
