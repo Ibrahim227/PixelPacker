4637dd6e92219140d9d76f92845a84e934935306
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-30 07:56:08 +0100

    update models/CompressionParameters

--- models/CompressionParameters.py
+++ models/CompressionParameters.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, String, Integer, Text, ForeignKey
+from sqlalchemy import Column, String, Integer, Text
 
-import models
 from models.base_model import BaseModel, Base
+import models
 
 
 class CompressionParameters(BaseModel, Base):
     """Compression parameters"""
-    __tablename__ = "compressionparameters"
-    compression_id = Column(Integer, primary_key=True)
-    image_id = Column(Integer)
-    algorithm = Column(String(255), nullable=False)
-    compression_level = Column(Integer, nullable=False)
-    quantization_matrix = Column(Text, nullable=False)
-    transform_method = Column(String(255), nullable=False)
+    if models.storage_t == 'db':
+        __tablename__ = "compressionparameters"
+        compression_id = Column(Integer, primary_key=True)
+        image_id = Column(Integer)
+        algorithm = Column(String(255), nullable=False)
+        compression_level = Column(Integer, nullable=False)
+        quantization_matrix = Column(Text, nullable=False)
+        transform_method = Column(String(255), nullable=False)
+    else:
+        compression_id = ""
+        image_id = ""
+        algorithm = ""
+        compression_level = ""
+        quantization_matrix = ""
+        transform_method = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize CompressionParameters"""
