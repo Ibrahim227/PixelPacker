304f5b9d702d822775915ed57671b51dac82377f
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-31 08:29:43 +0100

    add & update ChooFile

--- models/ChooseFile.py
+++ models/ChooseFile.py
@@ -1,10 +1,29 @@
-#!/usr/bin/python3
-"""Import the required libraries/modules"""
+#!/usr/bin/python
+import cv2
+from PIL import Image
 
 
-class ChooseFile:
-    """The ChooseFile class: the file to be compressed"""
+def choosefile():
+    """Choose a file to convert"""
+    # Define the path to the input image
+    input_image_path = "../web_static/images/logo2.png"
 
-    def __init__(self):
-        """Initialize the class"""
-        pass
+    # Open the image using Pillow
+    image = Image.open(input_image_path)
+
+    # Display basic properties of the image
+    print("Image format:", image.format)
+    print("Image mode:", image.mode)
+    print("Image size:", image.size)
+
+
+def display_image():
+    """Display the image"""
+    img = cv2.imread('../web_static/images/logo2.png')
+    cv2.imshow('logo2', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+choosefile()
+display_image()
