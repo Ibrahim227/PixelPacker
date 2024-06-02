f95fbdb4e053b0c320a32a3c648e8f46b9f4e208
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-06-02 16:38:03 +0100

    update route_App

--- web_flask/route_app.py
+++ web_flask/route_app.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """Import required libraries/modules"""
-from flask import Flask, render_template, request, redirect, url_for
-
-import web_static
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/web_static/')
+@app.route('/', strict_slashes=False)
 def index():
     return render_template('index.html')
 
 
-@app.route('/run-script', methods=['POST'])
+@app.route('/', strict_slashes=False)
+def history():
+    return render_template('history.html')
+
+
+@app.route('/run-script', methods=['GET'])
 def run_script():
     print('Running script...')
 
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+    app.run(host='0.0.0.0', port=8000)
