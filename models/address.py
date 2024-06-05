3aef50434d6dd8e369c4814c874be3136f841004
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-06-05 08:54:45 +0100

    update models/address

--- models/address.py
+++ models/address.py
@@ -3,25 +3,19 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
 
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class Address(BaseModel, Base):
     """Represents an address of a user"""
-    if models.storage_t == 'db':
-        __tablename__ = 'address'
-        street_id = Column(Integer, primary_key=True, autoincrement=True)
-        user_id = Column(Integer, ForeignKey('user.id'))
-        street = Column(Integer, nullable=False)
-        city = Column(String(255), nullable=False)
-        state = Column(String(255), nullable=False)
-        postal_code = Column(Integer, nullable=False)
-        user = relationship("User", back_populates="address")
-    else:
-        street_id = ""
-        street = ""
-        city = ""
-        state = ""
-        postal_code = ""
+    __tablename__ = 'address'
+    street_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    street = Column(Integer, nullable=False)
+    city = Column(String(255), nullable=False)
+    state = Column(String(255), nullable=False)
+    postal_code = Column(Integer, nullable=False)
+    user = relationship("User", back_populates="address")
 
     def __init__(self, *args, **kwargs):
         """Initialize the Address"""
