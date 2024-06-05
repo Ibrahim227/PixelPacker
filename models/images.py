97f157595e48461d1e39f3ce58aadef5925a1f1d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-06-05 08:54:59 +0100

    update models/images

--- models/images.py
+++ models/images.py
@@ -8,21 +8,14 @@ from models.base_model import BaseModel, Base
 
 class Images(BaseModel, Base):
     """Represents Images properties"""
-    if models.storage_t == 'db':
-        __tablename__ = 'images'
-        image_id = Column(Integer, primary_key=True)
-        original_filename = Column(String(255), nullable=False)
-        upload_date = Column(DateTime, nullable=False)
-        original_size = Column(Integer, nullable=False)
-        width = Column(Integer, nullable=False)
-        height = Column(Integer, nullable=False)
-        color_space = Column(String(255), nullable=False)
-    else:
-        image_id = ""
-        original_filename = ""
-        upload_date = ""
-        original_size = ""
-        color_space = ""
+    __tablename__ = 'images'
+    image_id = Column(Integer, primary_key=True)
+    original_filename = Column(String(255), nullable=False)
+    upload_date = Column(DateTime, nullable=False)
+    original_size = Column(Integer, nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    color_space = Column(String(255), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Initialize Images"""
