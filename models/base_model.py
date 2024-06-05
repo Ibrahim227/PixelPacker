f2bc5f03a8e610f6ed4537b94e8521ebcc28eb33
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-06-05 08:53:26 +0100

    update models/base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,18 @@ import uuid
 from datetime import datetime
 
 from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-import models
+from sqlalchemy.orm import declarative_base
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+Base = declarative_base()
 
 
 class BaseModel:
     """Main class for all models"""
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.now())
-        updated_at = Column(DateTime, default=datetime.now())
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -42,28 +36,3 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
-    def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        if save_fs is None:
-            if "password" in new_dict:
-                del new_dict["password"]
-        return new_dict
-
-    def delete(self):
-        """delete the current instance from the storage"""
-        models.storage.delete(self)
