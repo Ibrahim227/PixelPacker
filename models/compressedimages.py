752b07795686cb0250369b28595fa74f5f34f041
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-06-05 08:54:31 +0100

    update models/compressedimage

--- models/compressedimages.py
+++ models/compressedimages.py
@@ -8,21 +8,13 @@ from models.base_model import BaseModel, Base
 
 class CompressedImages(BaseModel, Base):
     """Represents the Compressed image"""
-    if models.storage_t == 'db':
-        __tablename__ = 'compressedimages'
-        compressed_images_id = Column(Integer, primary_key=True)
-        image_id = Column(Integer, nullable=False)
-        compressed_size = Column(Integer, nullable=False)
-        compressed_ratio = Column(Float, nullable=False)
-        compressed_filename = Column(String(255), nullable=False)
-        compressed_data = Column(BLOB, nullable=False)
-    else:
-        compressed_images_id = ""
-        image_id = ""
-        compressed_size = ""
-        compressed_ratio = ""
-        compressed_filename = ""
-        compressed_data = ""
+    __tablename__ = 'compressedimages'
+    compressed_images_id = Column(Integer, primary_key=True)
+    image_id = Column(Integer, nullable=False)
+    compressed_size = Column(Integer, nullable=False)
+    compressed_ratio = Column(Float, nullable=False)
+    compressed_filename = Column(String(255), nullable=False)
+    compressed_data = Column(BLOB, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Initialize CompressedImages"""
